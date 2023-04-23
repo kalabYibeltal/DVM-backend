877ed4a69f05e70460a9fd0bc5a97a2838f99f9f
kalab Yibeltal <kalabyib@gmail.com>
2023-04-23 18:32:25 +0300

    something

--- models/vmachine.js
+++ models/vmachine.js
@@ -1,34 +1,23 @@
 const { ObjectId } = require('bson')
 const mongoose = require('mongoose')
 
-const carSchema = new mongoose.Schema({
+const vmSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter the name of your car'],
         maxlength: [15, 'maximum character is 15']
     },
-    plateNumber:{
+    IdNumber:{
         type: String,
         required: [true, 'Please enter the plate number'],
         length: 6
     },
-    model:{
-        type: String,
+    items:{
+        type: List,
         required: [true, "Please enter the car model"]
     },
-    color:{
-        type: String,
-        required: [true, "Please enter the car color"]
-    },
-    ownerId:{
-        type: String,
-        required: [true, 'Please enter the owner id'],
-    },
-    region:{
-        type: String,
-        required: [true, 'Please enter the region'],
-    }
+    
 })
 
-const Car = mongoose.model('car', carSchema)
-module.exports = Car
+const Vm = mongoose.model('vm', vmSchema)
+module.exports = Vm
