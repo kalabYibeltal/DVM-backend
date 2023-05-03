32a893dafd01bd2522eaf3c3520d03abe4ade5c7
kalab Yibeltal <kalabyib@gmail.com>
2023-05-04 00:59:03 +0300

    addmachineError

--- controllers/vmController.js
+++ controllers/vmController.js
@@ -12,13 +12,16 @@ const createToken = (id)=>{
 
 module.exports.createmachine_post = async (req, res)=>{
     const {name, numberofitems, items, city, building} = req.body;
-    const income = 0;
+    const temp = {};
+    // for (i=0, i<)
+    // const income = 0;
     try{
         const machine = await Vm.create({ name, numberofitems,items, city, building,income})
         res.status(201).json({ machine })
 
     }catch(err){
         console.log(err)
+        res.status(400).json(req.body)
     }
 }
 
